6796d1fe2b84bc303e56022a004bf16f77f33205
Mark Azemoon <mmazemoon@gmail.com>
2015-05-14 12:40:20 -0700

    Add ::find_by_credentials method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
 
 validates :email, :password_digest, :session_token, presence: true
 validates :email, :session_token, uniqueness: true
+
+after_initialize :ensure_session_token
+
+def self.find_by_credentials(email, password)
+  user = self.find_by(email: email)
+  return nil if user.nil?
+  user.is_password?(password) ? user : nil
+end
 
 def self.generate_session_token
   SecureRandom::base64(16)
@@ -25,5 +45,9 @@ end
 def is_password?(password)
    BCrypt::Password.new(self.password_digest).is_password?
 end
+
+
+
+
 
 end
