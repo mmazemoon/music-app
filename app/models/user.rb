159a490b6f97e31dc3f10c2ef30290b4e915ff4e
Mark Azemoon <mmazemoon@gmail.com>
2015-05-14 11:14:38 -0700

    Add session token methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+
+validates :email, :password_digest, :session_token, presence: true
+validates :email, :session_token, uniqueness: true
+
+def self.generate_session_token
+  SecureRandom::base64(16)
+end
+
+def reset_session_token!
+  self.session_token= self.class.generate_session_token
+  self.save!
+  session_token
+end
+
+def ensure_session_token
+  self.session_token ||= self.reset_session_token!
+end
+
+
 end
